a769d9be08c29e1b9a7fd0c5ea2ce4e41ff01cc0
Wojtek <142600878+aszkiel71@users.noreply.github.com>
2024-10-03 18:54:54 +0200

    Create convert_real_to_binary_but_fractions.py

---
+++ convert_real_to_binary_but_fractions.py
@@ -0,0 +1,17 @@
+# tbh it's easiest than expected
+# algorithm from #0 to-do-list from University of Wrocław (Computer Sience) 
+
+# enter:
+# a -> real number, belongs to [0, 1)
+#
+# exit:
+# result -> our result
+
+a = 0.375 # f.i. 0.375
+
+print(0)
+print(".")
+while a != 0:
+    a *= 2
+    print(a//1)
+    a = a%1
